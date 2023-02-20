ef91fa767de09d160f58698479675409fe7a55af
rekhan <rekhan@expediagroup.com>
2023-02-20 13:31:01 +0000

    finished activity

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const app = express();
-const {Musician} = require("./Musician")
-const {sequelize} = require("./db")
+const { Musician } = require("./Musician");
+const { sequelize } = require("./db");
 
 const port = 3000;
 
 //TODO
 
 app.listen(port, () => {
-    sequelize.sync();
-    console.log(`Listening on port ${port}`)
-})
+  sequelize.sync();
+  console.log(`Listening on port ${port}`);
+});
+
+app.get("/musicians", async (req, res) => {
+  const AllMusicians = await Musician.findAll();
+  res.json(AllMusicians);
+});
